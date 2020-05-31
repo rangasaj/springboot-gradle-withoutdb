42e59dd6f5744d29e486f7d45ad77adea08c6442
Jaga <jagaranga@gmail.com>
2020-06-01 00:14:55 +0530

    Adding missing files

--- src/main/java/me/jaga/basics/springboot/gradle/dao/GreetingDAO.java
+++ src/main/java/me/jaga/basics/springboot/gradle/dao/GreetingDAO.java
@@ -1,4 +1,11 @@
 package me.jaga.basics.springboot.gradle.dao;
 
+import org.springframework.stereotype.Component;
+
+@Component
 public class GreetingDAO {
+
+    public String getGreetingMessage() {
+        return "Welcome Greetings!";
+    }
 }
